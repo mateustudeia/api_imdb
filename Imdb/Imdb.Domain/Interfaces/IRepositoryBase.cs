468fe867af02e85b4a8e3c6144061b715aceb1a3
Mateus <mateustudeia@gmail.com>
2020-11-12 18:24:02 -0300

    Adicionado a interface IRepositoryBase.cs para alterar logica de implementação do repositório

---
+++ Imdb/Imdb.Domain/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imdb.Domain.Interfaces
+{
+    public interface IRepositoryBase<TEntity>
+    {
+        void Insert(TEntity obj);
+        void Update(TEntity obj);
+        IEnumerable<TEntity> Select();
+        TEntity Select(int id);
+
+    }
+}
