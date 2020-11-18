5b5e5b07f361b2653c86bb3a6a4ad3f968f619d9
Mateus <mateustudeia@gmail.com>
2020-11-18 15:47:55 -0300

    add MevieModel.cs

---
+++ Imdb/Imdb.Domain/Models/MovieModel.cs
@@ -0,0 +1,35 @@
+﻿using Imdb.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imdb.Domain.Models
+{
+    public class MovieModel
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public int Ano { get; set; }
+        public string TempoDuracao { get; set; }
+        public string Enredo { get; set; }
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+        public string Atores { get; set; }
+
+        public MovieModel() { }
+        
+        public MovieModel(Movie movie)
+        {
+            Id = movie.Id;
+            Titulo = movie.Titulo;
+            Ano = movie.Ano;
+            TempoDuracao = movie.TempoDuracao.ToString();
+            Enredo = movie.Enredo;
+            Diretor = movie.Diretor;
+            Genero = movie.Genero;
+            Atores = movie.Atores;
+
+        }
+    }
+
+}
